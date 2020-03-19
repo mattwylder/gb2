1cb3f438fce5dd80c286e0d715112a4e20dd824b
Matt Wylder <matt.wylder@gpshopper.com>
2020-03-19 14:29:07 -0500

    added messaging to build script

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh 
 
 function clean_up {
+    echo "Cleanup! Removing object files..."
     rm *.o >/dev/null 2>&1
+    echo "Cleanup complete!"
 }
 
 function error {
+    echo "Build failed!"
    clean_up 
    exit 1
 }
@@ -12,9 +15,14 @@ function error {
 BASEDIR=`dirname $0`
 MAIN=$BASEDIR/src/main.asm
 
+echo "assembling..."
 rgbasm -o chapo.o $MAIN || error
+echo "assembling complete!"
+echo "linking..."
 rgblink -o $BASEDIR/chapo.gb chapo.o || error
+echo "linking complete!"
+echo "fixing gb file..."
 rgbfix -v $BASEDIR/chapo.gb || error
-
-echo "success"
+echo "fixing complete!"
+echo "build complete!"
 clean_up 
