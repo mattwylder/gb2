f297e7c27f5c3efdef2f370a8c013048d0e64d47
Matt Wylder <matt.wylder@gpshopper.com>
2020-03-19 16:31:33 -0500

    cleaning up some build script shit

--- build.sh
+++ build.sh
@@ -12,17 +12,24 @@ function error {
    exit 1
 }
 
+function pretty_print {
+    echo "****************************"
+    echo $1
+    echo "****************************"
+}
+
 BASEDIR=`dirname $0`
 MAIN=$BASEDIR/src/main.asm
+OUTPUT=$BASEDIR/result.gb
 
-echo "assembling..."
-rgbasm -o chapo.o $MAIN || error
-echo "assembling complete!"
+pretty_print "assembling..."
+rgbasm -o object.o $MAIN || error
+echo "assembling complete!\n"
 echo "linking..."
-rgblink -o $BASEDIR/chapo.gb chapo.o || error
-echo "linking complete!"
+rgblink -o $OUTPUT object.o || error
+echo "linking complete!\n"
 echo "fixing gb file..."
-rgbfix -v $BASEDIR/chapo.gb || error
-echo "fixing complete!"
+rgbfix -v $OUTPUT || error
+echo "fixing complete!\n"
 echo "build complete!"
 clean_up 
